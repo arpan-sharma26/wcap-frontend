7bdac99d4fcf74f3c6ae2ae281b262e5f9e2a3ae
Arpan Sharma <arpan.sharma26@gmail.com>
2021-02-22 17:07:56 +0530

    changes made

--- src/app/components/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RegisterService } from '../../register.service';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.css']
+  styleUrls: ['./log-in.component.css'],
 })
 export class LogInComponent implements OnInit {
+  public loginForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private serv: RegisterService) {
+    this.createForm();
   }
 
+  createForm() {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onLoginSubmit() {
+    console.log(this.loginForm.value);
+    this.serv.authenticateUser(this.loginForm.value);
+  }
+
+  ngOnInit(): void {}
 }
--- src/app/register.service.ts
+++ src/app/register.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -8,12 +9,20 @@ export class RegisterService {
   public authToken: any;
   user: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, public HttpHeaders: HttpHeaders) {}
 
   public ROOTURL = 'http://localhost:3000';
   public signupURL = `${this.ROOTURL}/users/register`;
+  public loginURL = `${this.ROOTURL}/users/authenticate`;
 
   signUp(userData: any) {
     return this.http.post<any>(this.signupURL, userData);
+  }
+
+  authenticateUser(user: any) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    alert('This is the sample message.');
+    return this.http.post(this.loginURL, user);
   }
 }
